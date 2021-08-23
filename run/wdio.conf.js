67809fc587066e526d980a5507853af6fcc45eb0
tractiongatherqa <amcmillan@tractionondemand.com>
2021-08-23 10:11:44 -0700

    ko

--- run/wdio.conf.js
+++ run/wdio.conf.js
@@ -76,12 +76,12 @@ exports.config = {
                 "profile.managed_default_content_settings.popups":2,
                 "profile.managed_default_content_settings.geolocation":2,
                 "profile.managed_default_content_settings.media_stream":2,
-            }
-            // args: ["--headless", 
-            // "--disable-gpu",
-            // "--disable-notifications",
-            // "--no-sandbox"
-            // ],
+            },
+            args: ["--headless", 
+            "--disable-gpu",
+            "--disable-notifications",
+            "--no-sandbox"
+            ],
 
         },
         
